91f567f99cb809cb9fe674c5b5069373828e22ad
zhouweibin <zhouweibin@163.com>
2016-02-17 14:44:40 +0800

    打印log1

--- haha/haha/ViewController.m
+++ haha/haha/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     NSLog(@"nishishui");
     NSLog(@"zhouweibin");
+    NSLog(@"log1111");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
